73a927b1cc06ea3f3047f2fa9fb840f766a57c7b
Exabait <v.step2327@gmail.com>
2020-12-04 16:46:55 +0200

    functionality and styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+
 import { TagService } from './services/tag.service';
 import { BoardsService } from './services/boards.service';
-import { FormControl } from '@angular/forms';
+
 import { BoardModel } from './models/board.model';
+
+import { OverviewDialogComponent } from './components/overview-dialog/overview-dialog.component';
+
+
 
 @Component({
   selector: 'app-root',
@@ -15,17 +22,89 @@ export class AppComponent implements OnInit {
   public boardsList: BoardModel[] = [];
   public board: BoardModel;
   public imageInput = new FormControl('');
+  public boardOptions = new FormControl('');
+  public isTagShowed = false;
 
   constructor(private boardsService: BoardsService,
-              private tagService: TagService) {
+              private tagService: TagService,
+              public dialog: MatDialog) {
   }
 
   ngOnInit(): void {
+   this.getBoards();
+  }
+
+  addImage(): void {
+    this.tagService.getImageTags(this.imageInput.value).subscribe(res => {
+      this.board.photos.push({
+        link: this.imageInput.value,
+        tags: res.result.tags.slice(0, 10),
+        dateCreated: Date.now()
+      });
+      this.imageInput.setValue('');
+      console.log(this.board);
+    });
+  }
+
+  showTags(): void {
+    this.isTagShowed = true;
+  }
+
+  changeBoard(): void {
+    this.boardsList.map(board => {
+      if (board.title === this.boardOptions.value){
+        this.board = board;
+      }
+    });
+    this.isTagShowed = false;
+  }
+
+  saveBoard(): void {
+    this.boardsService.updateBoard(this.board).subscribe(value => console.log(value));
+  }
+
+  dismissChanges(): void {
+    this.getBoards();
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(OverviewDialogComponent, {
+      width: '250px',
+      data: { boardTitle: this.board.title }
+    });
+
+    dialogRef.afterClosed().subscribe(title => {
+      if (!title) { return; }
+      if (this.isNewBoard(title)) {
+        this.board = {
+          title,
+          photos: []
+        };
+        this.boardsList.push(this.board);
+      } else {
+        this.boardsList.map(board => {
+          if (board.title === title) {
+            this.board = board;
+          }
+        });
+      }
+    });
+  }
+
+  private isNewBoard(title): boolean {
+    for (const board of this.boardsList) {
+      if (board.title === title) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  private getBoards(): void {
     this.boardsService.getAllBoards().subscribe(boards => {
       this.boardsList = boards;
       this.board = this.boardsList[0];
     });
   }
-
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatDialogModule} from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { BoardsService } from './services/boards.service';
 import { TagService } from './services/tag.service';
 
 import { AppComponent } from './app.component';
+import { OverviewDialogComponent } from './components/overview-dialog/overview-dialog.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OverviewDialogComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [BoardsService, TagService],
   bootstrap: [AppComponent]
